32201f8585ccd510e80c6c34f91735bd04761da0
Albert wang <awang21@ssis.edu.vn>
2019-08-26 10:52:42 +0700

    Update Table Reservation.py

--- Table Reservation.py
+++ Table Reservation.py
@@ -1,5 +1,11 @@
+#Albert
+#Asks for name 
 name = input("Name: ")
 reservation_name = "Albert"
+"""
+function that prints out "right this way" if the name == reservation_name,
+if not it prints out "sorry we dont have a reservation under that name.
+"""
 
 def check_name(name, reservation_name):
     if name == reservation_name:
